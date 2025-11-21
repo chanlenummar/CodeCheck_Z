4605d58c38081fdc7454ade7e1a07cca3d017533
chanlenummar <chanlenummar@users.noreply.github.com>
2025-11-21 09:00:40 +0000

    Add middleware-secure-global.yaml, registry-auth.sol, serializer-fixture-statistics.go

---
+++ frontend/web/src/lib-user-statistics/serializer-fixture-statistics.go
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/codecheck_z/frontend/web/src/lib-user-statistics/serializer-fixture-statistics.go
+// Generated at: 2025-11-27T15:10:54.111Z
+
+
